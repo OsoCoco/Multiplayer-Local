d2d81a3e0a2c96be0b7daed5f598e07e12be21a1
OsoCoco <61520900+OsoCoco@users.noreply.github.com>
2021-09-10 10:37:45 -0500

    Shooting

--- Redes2/Assets/Scripts/Tank.cs
+++ Redes2/Assets/Scripts/Tank.cs
@@ -4,20 +4,23 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 public class Tank : MonoBehaviour
 {
+    
     Vector2 move;
-    public float tankSpeed;
+
+    public GameObject bullet;
+    public Transform barrel;
+
+    public float tankSpeed,attackDamage,life;
+    public float fireRate = 0.5f;
     public float rotationSpeed = 720;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    float nextFire;
+
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(tankSpeed * move.y * Time.deltaTime * Vector3.forward);
+        transform.Translate(new Vector3(move.x,0,move.y) * tankSpeed * Time.deltaTime);
         Quaternion toRotation = Quaternion.LookRotation(new Vector3(move.x, 0, move.y), Vector3.up);
         transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
         //transform.Rotate(rotSpeed * move.x * Time.deltaTime * Vector3.up);
@@ -25,13 +28,20 @@ public class Tank : MonoBehaviour
 
     public void Fire(InputAction.CallbackContext ctx)
     {
-        Debug.Log("Fire");
+        if(ctx.performed && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            Instantiate(bullet, barrel.transform.position, barrel.transform.rotation);
+        }
+
+        
+        //Debug.Log("Fire");
     }
 
     public void Move(InputAction.CallbackContext ctx)
     {
         move = ctx.ReadValue<Vector2>();
         
-        //Debug.Log(ctx.ReadValue<Vector2>().x);
+        
     }
 }
