4b7f9a3ed074e4c83a744aed1722f715879d631a
OsoCoco <61520900+OsoCoco@users.noreply.github.com>
2021-09-20 15:04:19 -0500

    Cambios de Materiales y agregado de SpawnPoint

--- Redes2/Assets/Scripts/GameManager.cs
+++ Redes2/Assets/Scripts/GameManager.cs
@@ -5,13 +5,17 @@ using UnityEngine.InputSystem;
 
 public class GameManager : MonoBehaviour
 {
-
+    public List<Material> differentColors;
     public Transform[] spawnPoints;
     public int playerID;
     // Start is called before the first frame update
     public void OnSpawnPlayer(PlayerInput playerInput)
     {
+        Material rMat = differentColors[Random.Range(0, differentColors.Count)];
         playerInput.gameObject.transform.position = spawnPoints[playerID].position;
+        playerInput.gameObject.GetComponent<Tank>().ChangeColor(rMat);
+        playerInput.gameObject.GetComponent<Tank>().spawnPoint = spawnPoints[playerID];
+        differentColors.Remove(rMat);
         playerID++;
         Debug.Log("Se agrego al jugador: " + playerID);
     }
--- Redes2/Assets/Scripts/Tank.cs
+++ Redes2/Assets/Scripts/Tank.cs
@@ -2,18 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 public class Tank : MonoBehaviour
 {
     
+    public MeshRenderer[] renderToChange; 
     Vector2 move;
 
     public GameObject bullet;
     public Transform barrel;
 
+    public float score;
+
+    public Transform spawnPoint;
+    
     public float tankSpeed,attackDamage,life;
     public float fireRate = 0.5f;
     public float rotationSpeed = 720;
-
+    public TextMeshPro scoreText;
     float nextFire;
 
 
@@ -28,14 +34,31 @@ public class Tank : MonoBehaviour
 
     public void Fire(InputAction.CallbackContext ctx)
     {
-        if(ctx.performed && Time.time > nextFire)
+        /*if(ctx.performed && Time.time > nextFire)
         {
             nextFire = Time.time + fireRate;
             Instantiate(bullet, barrel.transform.position, barrel.transform.rotation);
-        }
+        }*/
 
-        
         //Debug.Log("Fire");
+    }
+
+    void Respawn()
+    {
+        Instantiate(this, spawnPoint.position, Quaternion.identity);
+    }
+
+    void Death()
+    {
+
+    }
+    
+    public void ChangeColor(Material material)
+    {
+        for (int i = 0; i < renderToChange.Length; i++)
+        {
+            renderToChange[i].materials[0] = material;
+        }
     }
 
     public void Move(InputAction.CallbackContext ctx)
